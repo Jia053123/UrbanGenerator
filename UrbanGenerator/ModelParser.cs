ad14c853d4997d603593f1023e7a87ee61fe1e6a
ErnestX <xiangspecial2012@gmail.com>
2024-04-15 17:56:23 -0400

    parse model

--- UrbanGenerator/ModelParser.cs
+++ UrbanGenerator/ModelParser.cs
@@ -17,15 +17,63 @@ namespace UrbanGenerator
             this.model = new XmlDocument();
             this.model.Load(pathToModel);
             this.nsManager = new XmlNamespaceManager(model.NameTable);
-            nsManager.AddNamespace("ns0", model.DocumentElement.GetNamespaceOfPrefix("ns0"));
+            nsManager.AddNamespace("ns0", this.model.DocumentElement.GetNamespaceOfPrefix("ns0"));
+        }
+
+        public int YearBuilt
+        {
+            get
+            {
+                XmlNode node = this.model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:BuildingSummary/ns0:BuildingConstruction/ns0:YearBuilt", this.nsManager);
+                int.TryParse(node.InnerText, out int yearBuilt);
+                return yearBuilt;
+            }
+        }
+
+        public string ResidentialFacilityType
+        {
+            get
+            {
+                XmlNode node = this.model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:BuildingSummary/ns0:BuildingConstruction/ns0:ResidentialFacilityType", this.nsManager);
+                return node.InnerText;
+            }
+        }
+
+        public int NumOfConditionedFloorsAboveGrade
+        {
+            get
+            {
+                XmlNode node = this.model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:BuildingSummary/ns0:BuildingConstruction/ns0:NumberofConditionedFloorsAboveGrade", this.nsManager);
+                int.TryParse(node.InnerText, out int numOfFloors);
+                return numOfFloors;
+            }
+        }
+
+        public float AverageCeilingHeight
+        {
+            get
+            {
+                XmlNode node = this.model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:BuildingSummary/ns0:BuildingConstruction/ns0:AverageCeilingHeight", this.nsManager);
+                float.TryParse(node.InnerText, out float averageCeilingHeight);
+                return averageCeilingHeight;
+            }
         }
 
         public XmlNodeList Walls
         {
             get
             {
-                XmlNode walls = model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:Enclosure/ns0:Walls", this.nsManager);
+                XmlNode walls = this.model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:Enclosure/ns0:Walls", this.nsManager);
                 return walls.ChildNodes;
+            }
+        }
+
+        public XmlNodeList Floors
+        {
+            get
+            {
+                XmlNode floors = this.model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:Enclosure/ns0:Floors", this.nsManager);
+                return floors.ChildNodes;
             }
         }
     }
