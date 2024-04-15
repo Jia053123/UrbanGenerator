7ba287855565c01a5d8b45667e351c52a5c9008c
ErnestX <xiangspecial2012@gmail.com>
2024-04-15 12:09:10 -0400

    get wall nodes

---
+++ UrbanGenerator/ModelParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace UrbanGenerator
+{
+    class ModelParser
+    {
+        private XmlDocument model;
+        //private string nameSpaceName;
+        private XmlNamespaceManager nsManager;
+        public ModelParser(string pathToModel)
+        {
+            this.model = new XmlDocument();
+            this.model.Load(pathToModel);
+            this.nsManager = new XmlNamespaceManager(model.NameTable);
+            nsManager.AddNamespace("ns0", model.DocumentElement.GetNamespaceOfPrefix("ns0"));
+        }
+
+        public XmlNodeList Walls
+        {
+            get
+            {
+                XmlNode walls = model.DocumentElement.SelectSingleNode("/ns0:HPXML/ns0:Building/ns0:BuildingDetails/ns0:Enclosure/ns0:Walls", this.nsManager);
+                return walls.ChildNodes;
+            }
+        }
+    }
+}
